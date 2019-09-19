ae4d194becbdd49912339f3adab960bd4e3cdc56
Rahul Singh <rahul.singh03@nagarro.com>
2019-09-19 16:53:34 +0530

    new test case

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-
 import {shallow} from 'enzyme';
 import App from './../App'
+import config from './components/configuartion';
 
 function setup(){
 const enzymeWrapper = shallow(<App/>)
@@ -17,9 +12,7 @@ return enzymeWrapper;
 describe("test app component",()=>{
   it('it should have one div',()=>{
     const enzymeWrapper = setup();
-   
     expect(enzymeWrapper.find('#mainDiv').length).toBe(1);
-    console.log(enzymeWrapper);
   })
 })
 
